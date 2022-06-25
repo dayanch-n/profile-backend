4a54a32527cec69ac5e742c661143a4fbd691b1d
iamdayanch <dayanch-iam@gmail.com>
2022-06-26 02:07:09 +0800

    setup project structure and setup

---
+++ helpers/database.js
@@ -0,0 +1,27 @@
+import mongodb from "mongodb";
+
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+export const mongoConnect = async () => {
+  try {
+    const client = await MongoClient.connect(
+      `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.ynx918k.mongodb.net/?retryWrites=true&w=majority`,
+      {
+        keepAlive: true,
+      }
+    );
+    console.log("Connected to DB");
+    _db = client.db();
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getDB = () => {
+  if (_db) {
+    return _db;
+  }
+  throw new Error("Database not found");
+};
