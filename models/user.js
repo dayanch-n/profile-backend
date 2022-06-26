3ae983ee0c56a0190cf105139e36fa3167cedb30
iamdayanch <dayanch-iam@gmail.com>
2022-06-26 16:49:31 +0800

    added user model schema

---
+++ models/user.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import crypto from "crypto";
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  dob: {
+    type: String,
+    required: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
+  salt: String,
+});
+
+// Method to set hash the password for a user
+userSchema.methods.hashedPassword = function (password) {
+  this.salt = crypto.randomBytes(16).toString("hex");
+  this.password = crypto.pbkdf2Sync(password, this.salt, 1000, 64, "sha512").toString("hex");
+};
+
+userSchema.methods.validPassword = function (password) {
+  const hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, "sha512").toString("hex");
+  return this.password === hash;
+};
+
+export default mongoose.model("User", userSchema);
